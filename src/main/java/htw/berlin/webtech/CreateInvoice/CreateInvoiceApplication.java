49be6c9d5060dd3bc0e5d264c2cdf82c354b18f8
Ungerm23 <mark.unger@student.htw-berlin.de>
2022-04-25 23:20:40 +0200

    init

---
+++ src/main/java/htw/berlin/webtech/CreateInvoice/CreateInvoiceApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.CreateInvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateInvoiceApplication.class, args);
+	}
+
+}
